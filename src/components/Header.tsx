56e14f63dea08377fd98a00e1a71f7c9aea0262a
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-07 14:24:37 +0300

    Update: Align Connect Wallet button with avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-start p-4">
+    <div className="flex justify-between items-center p-4">
       <div className="flex items-center gap-3">
         <div className="w-12 h-12 bg-white rounded-full overflow-hidden">
           {/* Здесь будет аватарка пользователя */}
